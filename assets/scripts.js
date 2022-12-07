2dfb6a76158ec346acbb1e3f6a9b7f16b77fedcf
Olivia Giandrea <og102@mit.edu>
2022-12-07 15:34:26 -0500

    add css shadow to modal

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,15 +15,15 @@ $("#green-btn-wrapper").on("click", function(event) {
 
   // if you can scroll in the modal, adjust the styles to accommodate a scrollbar
   if (modal.scrollHeight >= modal.clientHeight) {
-    $("#about-modal .close").css("right", "-76vh");
+    $("#about-modal .close").css("right", "-59vh");
 
     $("#about-modal .modal-content").css("top", "-1vh");
-    $("#about-modal .modal-content").css("left", "8.8vh");
+    $("#about-modal .modal-content").css("left", "5vh");
     $("#about-modal .modal-content").css("height", "53vh");
     $("#about-modal .modal-content").css("width", "60vh");
     $("#about-modal .modal-content").css("padding-top", "10vh");
     $("#about-modal .modal-content").css("padding-bottom", "10vh");
-    $("#about-modal .modal-content").css("padding-right", "15vh");
+    $("#about-modal .modal-content").css("padding-right", "9vh");
   }
 });
 
@@ -35,15 +35,15 @@ $("#orange-btn-wrapper").on("click", function(event) {
 
   // if you can scroll in the modal, adjust the styles to accommodate a scrollbar
   if (modal.scrollHeight <= modal.clientHeight) {
-    $("#maya-modal .close").css("right", "-76vh");
+    $("#maya-modal .close").css("right", "-59vh");
 
     $("#maya-modal .modal-content").css("top", "-1vh");
-    $("#maya-modal .modal-content").css("left", "8.8vh");
+    $("#maya-modal .modal-content").css("left", "5vh");
     $("#maya-modal .modal-content").css("height", "53vh");
     $("#maya-modal .modal-content").css("width", "60vh");
     $("#maya-modal .modal-content").css("padding-top", "10vh");
     $("#maya-modal .modal-content").css("padding-bottom", "10vh");
-    $("#maya-modal .modal-content").css("padding-right", "15vh");
+    $("#maya-modal .modal-content").css("padding-right", "9vh");
   }
 });
 
@@ -55,15 +55,15 @@ $("#brown-btn-wrapper").on("click", function(event) {
 
   // if you can scroll in the modal, adjust the styles to accommodate a scrollbar
   if (modal.scrollHeight <= modal.clientHeight) {
-    $("#bannerlord-modal .close").css("right", "-76vh");
+    $("#bannerlord-modal .close").css("right", "-59vh");
 
     $("#bannerlord-modal .modal-content").css("top", "-1vh");
-    $("#bannerlord-modal .modal-content").css("left", "8.8vh");
+    $("#bannerlord-modal .modal-content").css("left", "5vh");
     $("#bannerlord-modal .modal-content").css("height", "53vh");
     $("#bannerlord-modal .modal-content").css("width", "60vh");
     $("#bannerlord-modal .modal-content").css("padding-top", "10vh");
     $("#bannerlord-modal .modal-content").css("padding-bottom", "10vh");
-    $("#bannerlord-modal .modal-content").css("padding-right", "15vh");
+    $("#bannerlord-modal .modal-content").css("padding-right", "9vh");
   }
 });
 
@@ -75,15 +75,15 @@ $("#purple-btn-wrapper").on("click", function(event) {
   
   // if you can scroll in the modal, adjust the styles to accommodate a scrollbar
   if (modal.scrollHeight <= modal.clientHeight) {
-    $("#rpg-modal .close").css("right", "-76vh");
+    $("#rpg-modal .close").css("right", "-59vh");
 
     $("#rpg-modal .modal-content").css("top", "-1vh");
-    $("#rpg-modal .modal-content").css("left", "8.8vh");
+    $("#rpg-modal .modal-content").css("left", "5vh");
     $("#rpg-modal .modal-content").css("height", "53vh");
     $("#rpg-modal .modal-content").css("width", "60vh");
     $("#rpg-modal .modal-content").css("padding-top", "10vh");
     $("#rpg-modal .modal-content").css("padding-bottom", "10vh");
-    $("#rpg-modal .modal-content").css("padding-right", "15vh");
+    $("#rpg-modal .modal-content").css("padding-right", "9vh");
   }
 });
 
